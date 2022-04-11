6117666570e83af11ff1d36e42d3ad4041ef9253
Santiago Velosa <santiagovelosa@gmail.com>
2022-04-11 17:13:39 -0500

    Finish project setup

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,19 @@
+# frozen_string_literal: true
+
 # Load the Rails application.
-require_relative "application"
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp 
+ActionMailer::Base.perform_deliveries = true 
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.gmail.com' ,
+  port: 465, 
+  domain: 'gmail.com', 
+  user_name: 'therailscookbook@gmail.com', 
+  password: 'cookbook2022@', 
+  authentication: 'plain',
+  :ssl => true, :tsl => true, enable_starttls_auto: true
+}
