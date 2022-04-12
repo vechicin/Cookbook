104b8de4d570987deb5c8d269b7f629870b92205
Lucas-Masaba <masabaluke@gmail.com>
2022-04-13 02:24:19 +0300

    generate ability file

---
+++ app/models/ability.rb
@@ -0,0 +1,38 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # Define abilities for the passed in user here. For example:
+    #
+    can :read, Recipe
+
+    return unless user.present? # additional permissions for logged in users (they can read their own posts)
+
+    # can :create, [Post, Comment], user_id: user.id
+    can :destroy, [Recipe], user_id: user.id
+
+    return unless user.admin? # additional permissions for administrators
+
+    # can :create, [Post, Comment]
+    can :destroy, [Recipe]
+
+    #
+    # The first argument to `can` is the action you are giving the user
+    # permission to do.
+    # If you pass :manage it will apply to every action. Other common actions
+    # here are :read, :create, :update and :destroy.
+    #
+    # The second argument is the resource the user can perform the action on.
+    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
+    # class of the resource.
+    #
+    # The third argument is an optional hash of conditions to further filter the
+    # objects.
+    # For example, here the user can only update published articles.
+    #
+    #   can :update, Article, :published => true
+    #
+    # See the wiki for details:
+    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+  end
+end
