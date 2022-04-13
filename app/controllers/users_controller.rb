185e925d24a2deecdee992ba03c8ba30a772ca81
Lucas-Masaba <masabaluke@gmail.com>
2022-04-14 02:01:58 +0300

    fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  # before_action :set_user, only: %i[show edit update destroy]
+  before_action :set_user, only: %i[show edit update destroy]
 
   # GET /users or /users.json
   def index
@@ -59,9 +59,9 @@ class UsersController < ApplicationController
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   # Only allow a list of trusted parameters through.
   def user_params
