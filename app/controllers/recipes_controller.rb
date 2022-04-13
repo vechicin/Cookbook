84bbf8caa1fbec06a3aee0ddc7301eec9c9e8a5a
Lucas-Masaba <masabaluke@gmail.com>
2022-04-14 01:48:05 +0300

    fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: %i[show edit update destroy]
-  # load_and_authorize_resource
+  load_and_authorize_resource
   # GET /recipes or /recipes.json
   def index
     @recipes = Recipe.all
