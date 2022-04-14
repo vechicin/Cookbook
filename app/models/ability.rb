6bbabc3d1b9dae55e0f90e2cf8b0462cb95d4128
Santiago Velosa <santiagovelosa@gmail.com>
2022-04-13 19:44:21 -0500

    Fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
-    
+
     user ||= User.new # guest user (not logged in)
     if user.role == 'admin'
       can :manage, :all
